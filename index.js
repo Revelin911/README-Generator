41c0bfb2ffd00bdb18cb529385aacf45f97006f5
Revelin911 <revelin911@mail.com>
2024-12-15 16:01:10 -0700

    debugged index.js badges

--- index.js
+++ index.js
@@ -8,54 +8,56 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What is your project title?',
-     },
-{
-    type:'input',
-    name: 'description',
-    message: 'Provide a short description of your project using questions such as, what, why, and how?',
-},
-{
-    type: 'input',
-    name: 'installation',
-message: 'What steps are required to install?',
-},
-{
-    type: 'input',
-    name: 'usage',
-    message: 'What are the instructions to follow for usage?',
-},
-{
-type: 'input',
-name: 'contributions',
-message: 'Can you contribute?'
-},
+    },
     {
-     type: 'input',
-    name: 'tests',
-    message: 'What tests are available for the application to run?',
-},
-{
-    type: 'list',
-    name: 'license',
-    message: 'What license is being used?',
-    choices: ['MIT', 'GPLv2', 'Apache', 'Other']
-},
-{
-    type: 'input',
-    name: 'username',
-    message: 'What is your Guthub username',
-},
-{
-type: 'input',
-name: 'email',
-message: 'What is your email address?',
-},
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project using questions such as, what, why, and how?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What steps are required to install?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the instructions to follow for usage?',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Can you contribute?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What tests are available for the application to run?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license is being used?',
+        choices: ['MIT', 'GPLv2', 'Apache', 'Other']
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Guthub username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
 ];
 
-const generateReadMe = (answers) => 
-    {const licenseBadge =
-    `` //enter badge info here
-    `
+const generateReadMe = (answers) => {
+    const licenseBadge = `![badge](https://img.shields.io/badge/LICENSE-${answers.license}-blue)` //enter badge info here
+        
+    
+   return `
+${licenseBadge}
 # ${answers.title}
 
 ## Description
@@ -90,25 +92,24 @@ For any questions, please reference the information below:
 - Email:
 ${answers.email}
 `
-    }
+}
 
-    
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile('SREADME.md', readmeContent, (err) =>
+function writeToFile(fileName, readmeContent) {
+    fs.writeFile(fileName, readmeContent, (err) =>
         err ? console.log(err) : console.log('Successfully created README file!')
-      );
+    );
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt(questions).then((answers) => {
-    const readmeContent = 
-    generateReadMe(answers);
-    writeToFile('SREADME.md', readmeContent);
-});
+        .prompt(questions).then((answers) => {
+            const readmeContent = generateReadMe(answers);
+            writeToFile('SREADME.md', readmeContent);
+        });
 }
 
 // Function call to initialize app
